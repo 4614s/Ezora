27f63b49b6a643418fcdf61d45ea4295ceccd02d
4614s <onurgral1446.2@gmail.com>
2025-08-02 17:15:40 +0300

    Create cdntags.js

---
+++ unavaible-files/cdntags.js
@@ -0,0 +1,3 @@
+let cdnIjs = `  <script src="https://cdn.jsdelivr.net/gh/4614s/EzoraDOM-the-functions/CDN/i.js"></script>`;
+
+let cdnv1js = ` <script src="https://cdn.jsdelivr.net/gh/4614s/EzoraDOM-the-functions/CDN/v1.js"></script>`;
