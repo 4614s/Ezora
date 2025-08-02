8a9bb999355763eb7aec6cd5b0e50af03be4e7d9
4614s <onurgral1446.2@gmail.com>
2025-08-02 16:47:15 +0300

    Create footer.js

---
+++ unavaible-files/footer.js
@@ -0,0 +1,8 @@
+document.querySelector('ezs-footer').innerHTML = `
+©${new Date().getFullYear()} <span onclick="location.href='https://ezora.pages.dev'" style="cursor:pointer;">Ezora™</span> – <a href="https://github.com/4614s/Ezora/tree/main/LICENSE.md" style="color: white;">Ezora Halk Lisansı v1</a>
+`;
+
+document.querySelector('ezs-footer').style.textAlign="center";
+document.querySelector('ezs-footer').style.margin="20px";
+document.querySelector('ezs-footer').style.color="white";
+
