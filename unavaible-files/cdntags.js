8b213af176fbd8ffbfbc4d86f6aa9863bfe0aeb9
4614s <onurgral1446.2@gmail.com>
2025-08-16 23:30:47 +0300

    Update cdntags.js

--- unavaible-files/cdntags.js
+++ unavaible-files/cdntags.js
@@ -1,6 +1,6 @@
 let cdnTagezoradomIjs = `<script src="https://ezoradom.js.org/CDN/i.js"></script>`;
 let cdnTagezoradomV1js = `<script src="https://ezoradom.js.org/CDN/v1.js"></script>`;
-
+let cdnTagezoradomV1fastjs = `<script src="https://ezoradom.js.org/CDN/v1.fast.js"></script>`;
 
 let cdnIjs = cdnTagezoradomIjs;
 let cdnv1js = cdnTagezoradomV1js;
