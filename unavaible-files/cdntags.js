d90c75f088384eed71a1e80ff32ba89f5e52af17
4614s <onurgral1446.2@gmail.com>
2025-09-06 20:23:52 +0300

    Update cdntags.js

--- unavaible-files/cdntags.js
+++ unavaible-files/cdntags.js
@@ -3,6 +3,10 @@ let cdnTagezoradomV1js = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezorado
 let cdnTagezoradomV1fastjs = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@main/CDN/v1.fast.js"></script>`;
 let cdnTagezoradomv11js = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@main/CDN/v1.1.js"></script>`;
 let cdnTagezoradomv11fastjs = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@main/CDN/v1.1.fast.js"></script>`;
+let cdnTagezoradomOov1js = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@cdn/v1o.js"></script>`;
+let cdnTagezoradomOov1inspectjs = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@cdn/v1o.inspect.js"></script>`;
+let cdnTagezoradomOov11js = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@cdn/v1.1o.js"></script>`;
+let cdnTagezoradomOov11inspectjs = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@cdn/v1.1o.inspect.js"></script>`;
 
 
 let cdnIjs = cdnTagezoradomIjs;
