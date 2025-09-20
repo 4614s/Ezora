ada4708253aa8b5b3fa3636ebde2e63b62b0fa9d
4614s <onurgral1446.2@gmail.com>
2025-09-21 00:18:22 +0300

    Update footer.js

--- unavaible-files/footer.js
+++ unavaible-files/footer.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded",()=>{
 let lang = navigator.language;
-let licenseName; 
-lang.startsWith("tr") ? licenseName = "Ezora Halk Lisansı v1" : lang.startsWith("az") ? licenseName = "Ezora İctimai Lisenziyası v1" : licenseName = "Ezora Public License v1";
+let licenseName = lang.startsWith("tr") ? "Ezora Halk Lisansı v1" : lang.startsWith("az") ? "Ezora İctimai Lisenziyası v1" : "Ezora Public License v1";
   
 document.querySelector('.ezs-footer').innerHTML = `
-©${new Date().getFullYear()} <span onclick="location.href='https://ezora.pages.dev'" style="cursor:pointer;">Ezora™</span> – <a href="https://github.com/4614s/Ezora/tree/main/LICENSE.md" style="color: white;">${licenseName}</a>
+©${new Date().getFullYear()} <span onclick="location.href='https://ezora.pages.dev'" style="cursor:pointer;">Ezora™</span> – <a href="https://github.com/4614s/Ezora/tree/main/LICENSE.md" style="color: white; text-decoration: none;">${licenseName}</a>
 `;
 
 document.querySelector('.ezs-footer').style.textAlign="center";
