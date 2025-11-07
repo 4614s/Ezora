924a824a8b913f2931a4b93e61403eebbf37d25f
4614s <onurgral1446.2@gmail.com>
2025-11-07 18:46:02 +0300

    Change var declarations to const in cdntags.js

--- unavaible-files/cdntags.js
+++ unavaible-files/cdntags.js
@@ -1,13 +1,13 @@
-let cdnTagezoradomIjs = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@main/CDN/i.js"></script>`;
-let cdnTagezoradomV1js = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@main/CDN/v1.js"></script>`;
-let cdnTagezoradomV1fastjs = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@main/CDN/v1.fast.js"></script>`;
-let cdnTagezoradomv11js = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@v1.1/CDN/v1.1.js"></script>`;
-let cdnTagezoradomv11fastjs = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@v1.1/CDN/v1.1.fast.js"></script>`;
-let cdnTagezoradomOov1js = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@cdn/v1o.js"></script>`;
-let cdnTagezoradomOov1inspectjs = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@cdn/v1o.inspect.js"></script>`;
-let cdnTagezoradomOov11js = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@cdn/v1.1o.js"></script>`;
-let cdnTagezoradomOov11inspectjs = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@cdn/v1.1o.inspect.js"></script>`;
+const cdnTagezoradomIjs = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@main/CDN/i.js"></script>`;
+const cdnTagezoradomV1js = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@main/CDN/v1.js"></script>`;
+const cdnTagezoradomV1fastjs = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@main/CDN/v1.fast.js"></script>`;
+const cdnTagezoradomv11js = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@v1.1/CDN/v1.1.js"></script>`;
+const cdnTagezoradomv11fastjs = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@v1.1/CDN/v1.1.fast.js"></script>`;
+const cdnTagezoradomOov1js = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@cdn/v1o.js"></script>`;
+const cdnTagezoradomOov1inspectjs = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@cdn/v1o.inspect.js"></script>`;
+const cdnTagezoradomOov11js = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@cdn/v1.1o.js"></script>`;
+const cdnTagezoradomOov11inspectjs = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezoradom-the-functions@cdn/v1.1o.inspect.js"></script>`;
+const cdnTagEzfvsV1Min = `<script src="https://cdn.jsdelivr.net/gh/4614s/ezora-fvs@cdn/v1.ezorafvs.min.js"></script>`;
 
-
-let cdnIjs = cdnTagezoradomIjs;
-let cdnv1js = cdnTagezoradomV1js;
+const cdnIjs = cdnTagezoradomIjs;
+const cdnv1js = cdnTagezoradomV1js;
